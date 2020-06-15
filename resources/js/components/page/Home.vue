709471b01105a55255f946b2142360f060861ee1
hiwa1223 <57945054+hiwa1223@users.noreply.github.com>
2020-06-15 17:38:56 +0900

    レイアウトを分割する（ヘッダー）

--- resources/js/components/page/Home.vue
+++ resources/js/components/page/Home.vue
@@ -1,112 +1,73 @@
 <template>
   <div>
-    <header>
-      <div class="container text-center">
-        <h1>4 Answers Quiz 4択クイズ&クイズ徹底解説</h1>
-        <a href="/">
-          <img class="header-nav__logo" src="/public/images/header-logo.png" />
-        </a>
-      </div>
-      <nav class="navbar navbar-default navbar-static-top">
-        <div class="container">
-          <div class="navbar-header">
-            <button
-              type="button"
-              class="navbar-toggle"
-              data-toggle="collapse"
-              data-target="#navbar-ex-collapse"
-            >
-              <span class="sr-only">Toggle navigation</span>
-              <span class="icon-bar"></span>
-              <span class="icon-bar"></span>
-              <span class="icon-bar"></span>
-            </button>
-            <a class="navbar-brand" id="header-nav__home" href="/">ホーム</a>
-          </div>
-          <div class="collapse navbar-collapse" id="navbar-ex-collapse">
-            <ul class="nav navbar-nav navbar-right">
-              <li>
-                <a id="header-nav__login" href="/login">ログイン</a>
-              </li>
-              <li>
-                <a id="header-nav__register" href="/register">ユーザー登録</a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
-    </header>
+    <the-header></the-header>
     <main>
       <div class="container">
         <article class="col-md-8 col-xs-12">
-          <section>
-            <h2 class="quiz-question-h2">
-              <img class="quiz-question__logo" src="public/images/what-is-mark.png" />
-              第1問
+          <section class="home-quiz__introduction">
+            <h2 class="home-quiz__introduction-h2">
+              <img class="home-quiz__introduction-h2-logo" src="/public/images/what-is-mark.png" />4 Answers Quizとは?
             </h2>
-            <p>正しい敬語を使った表現を１つ選んでください。</p>
-            <div class="quiz-answer__list">
-              <ul>
-                <li>
-                  <a>
-                    <button>1</button>
-                  </a>
-                  受付でうかがってください。
-                </li>
-                <li>
-                  <a>
-                    <button>2</button>
-                  </a>
-                  課長がおっしゃったように、ファイルをご覧ください。
-                </li>
-                <li>
-                  <a>
-                    <button>3</button>
-                  </a>
-                  部長が申されたように進めていきます。
-                </li>
-                <li>
-                  <a>
-                    <button>4</button>
-                  </a>
-                  ○△商事の□□様がお越しになられました。
-                </li>
-              </ul>
-            </div>
+            <p>4 Answers Quizとはビジネスマナーから一般常識に至るまで様々なクイズを4択で出題するWEBアプリです。</p>
+            <p>何度もトライしてみて正解率100%を目指してみてください。</p>
           </section>
-          <section>
-            <h2 class="quiz-correct-h2">
-              <img class="quiz-correct__logo" src="public/images/correct-mark.png" />正解
+          <section class="home-quiz__setting">
+            <h2 class="home-quiz__setting-h2">
+              <img class="home-quiz__setting-h2-logo" src="/public/images/directory-icon.png" />出題設定
             </h2>
-            <p>
-              <button class="quiz-correct-answer">1</button>
-            </p>
-            <button>正解を表示する</button>
-            <div class="alert alert-info">
-              <strong>正解!</strong>
-            </div>
-            <div class="alert alert-danger">
-              <strong>不正解!</strong>
-            </div>
+            <form action="/quiz" method="post">
+              <label>
+                <input type="checkbox" name="categories[]" value="1" checked />ビジネスマナー
+              </label>
+              <label>
+                <input type="checkbox" name="categories[]" value="2" />一般常識
+              </label>
+              <label>
+                <input type="checkbox" name="categories[]" value="3" />就職・転職
+              </label>
+              <label>
+                <input type="checkbox" name="categories[]" value="4" />法律
+              </label>
+              <label>
+                <input type="checkbox" name="categories[]" value="5" />IT
+              </label>
+              <label>
+                <input type="checkbox" name="categories[]" value="6" />雑学
+              </label>
+              <div class>
+                全項目チェック
+                <button type="button" name="check_all" id="check-all" value="1">ON</button>
+                <button type="button" name="check_all_off" id="check-all-off" value="1">OFF</button>
+              </div>
+              <button type="submit" class="btn btn-primary">出題開始</button>
+              <input type="hidden" name="_token" value />
+            </form>
           </section>
-          <section >
-            <h2 class="quiz-commentary-h2">
-              <img class="quiz-commentary__logo" src="public/images/commentary-mark.png" />解説
+          <section class="home-quiz__ranking">
+            <h2 class="home-quiz__ranking-h2">
+              <img class="home-quiz__ranking-h2-logo" src="/public/images/graph-icon.png" />ランキング
             </h2>
-            <div class="quiz-commentary__text">
-              1）3）4）は、どこが間違っていたの？ <br>
-              1）受付でうかがってください。<br>
-              「うかがう」は謙譲語。謙譲語は自分または身内（自分の会社も含みます）の者に使う言葉で、相手に使うのは間違いです。<br>
-              『受付でお尋ねください。』が正解です。<br><br>
-              3）部長が申されたように進めていきます。<br>
-              「申す」も謙譲語です。謙譲語にれる・られるを付けても尊敬語にはなりません。<br>
-              『社長がおっしゃったように進めていきます。』が正解です。<br><br>
-              4）○△商事の□□様がお越しになられました。<br>
-              「なられました」は二重敬語の典型的な表現です。<br>
-              『○△商事の□□様がお越しになりました。』が正解です。<br>
+            <div>
+              <label>
+                <input class="ranking-radio" type="radio" name="ranking-radio" value="1" checked />総合
+              </label>
+              <label>
+                <input class="ranking-radio" type="radio" name="ranking-radio" value="2" />今月
+              </label>
+              <label>
+                <input class="ranking-radio" type="radio" name="ranking-radio" value="3" />今週
+              </label>
             </div>
-            <button type="button" class="btn btn-primary center-block">次の問題へ</button>
-            <button type="button" class="center-block">結果を見る</button>
+            <div class="home_quiz__ranking-chart"></div>
+          </section>
+          <section class="home__notice">
+            <h2 class="home__notice-h2">
+              <img class="home__notice-h2-logo" src="/public/images/news-icon.png" />お知らせ情報
+            </h2>
+            <dl>
+              <dt>2019/08/23</dt>
+              <dd>サイトを開設しました。</dd>
+            </dl>
           </section>
         </article>
         <aside class="col-md-4 col-xs-12">
@@ -238,11 +199,18 @@
         </aside>
       </div>
     </main>
-
-    <footer>
-      <div class="container text-center">
-        <small>© 2019 4Answers-quiz. All rights reserved.</small>
-      </div>
-    </footer>
+   <the-footer></the-footer>
   </div>
 </template>
+
+<script>
+import TheHeader from "../layout/TheHeader";
+import TheFooter from "../layout/TheFooter";
+
+export default {
+  components: {
+    TheHeader,
+    TheFooter,
+  }
+};
+</script>
